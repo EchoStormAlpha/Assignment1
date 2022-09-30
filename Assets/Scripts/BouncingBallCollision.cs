5bb6354c202f96d988a6d64bd0183a599c2884de
EchoStormAlpha <hunter.lippert@trojans.dsu.edu>
2022-09-30 14:18:46 -0500

    Added new script

---
+++ Assets/Scripts/BouncingBallCollision.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncingBallCollision : MonoBehaviour
+{
+    private bool bFlipDirection;
+
+    // Update is called once per frame
+
+    void Update()
+
+    {
+
+        if (bFlipDirection)
+
+            transform.position += Vector3.forward * Time.deltaTime * 5;
+
+        else
+
+            transform.position += Vector3.back * Time.deltaTime * 5;
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+
+    {
+
+        if (bFlipDirection) bFlipDirection = false;
+
+        else bFlipDirection = true;
+
+    }
+}
